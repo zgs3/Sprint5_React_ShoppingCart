4c833f369f5200ba3265c5bf9c89a15cf1227eb4
zgs3 <zkairaitis@gmail.com>
2022-05-18 17:17:34 +0300

    Added functionality for creating new items and rendering them into the list.

--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -4,21 +4,40 @@ import EditIcon from '@material-ui/icons/Edit';
 import DeleteForeverIcon from '@material-ui/icons/DeleteForever';
 
 function Cart() {
+
+  const [itemValue, setItemValue] = useState('');
+  const [items, setItems] = useState([]);
+
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    const newItem = {
+      id: Date.now().toString(),
+      name: itemValue,
+      checked: false
+    }
+    setItems([...items, newItem]);
+    setItemValue('');
+  }
+
   return (
     <div className={styles.container}>
       <div className={styles.itemsFull}>
         <ul className={styles.itemList}>
-          <div>
 
-            <div className={styles.item}>
-              <input type="checkbox" id="task-1" />
-              <label htmlFor="task-1">
+          {items.map((item) => (
+            <div key={item.id} className={styles.item}>
+              <input type="checkbox" id={item.id} />
+              <label htmlFor={item.id}>
                 <span className={styles.customCheckbox}></span>
-                Item 1
+                {item.name}
               </label>
               <div>
                 <button
-                  className={styles.itemBtn}>
+                  className={styles.itemBtn}
+                  onClick={(e) => {
+                    console.log(e.target);
+                  }} >
                   <EditIcon />
                 </button>
                 <button
@@ -27,33 +46,18 @@ function Cart() {
                 </button>
               </div>
             </div>
+          )
+          )}
 
-            <div className={styles.item}>
-              <input type="checkbox" id="task-1" />
-              <label htmlFor="task-1">
-                <span className={styles.customCheckbox}></span>
-                Item 2
-              </label>
-              <div>
-                <button
-                  className={styles.itemBtn}>
-                  <EditIcon />
-                </button>
-                <button
-                  className={styles.itemBtn}>
-                  <DeleteForeverIcon />
-                </button>
-              </div>
-            </div>
-
-          </div>
         </ul>
 
-        <form onSubmit={(e) => (e.preventDefault())}>
+        <form onSubmit={submitForm}>
           <input
             type="text"
             className={styles.newList}
             placeholder="New item"
+            onChange={(e) => { setItemValue(e.target.value) }}
+            value={itemValue}
           />
           <button className={styles.btn}>+</button>
         </form>
